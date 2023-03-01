9a2e9b6b220007218bf35197904e976247e9ba10
Kar Rui Lau <karrui@open.gov.sg>
2023-03-01 17:33:39 +0800

    feat: found the missing comma

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -360,11 +360,11 @@ U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA
 U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_SYM,       U_NA,              &kp PIPE,          &kp EXCL,          &kp AT,            &kp HASH,          &kp TILDE,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
 
-#define MIRYOKU_ALTERNATIVES_SYM \
-&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
-U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+// #define MIRYOKU_ALTERNATIVES_SYM \
+// &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+// &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+// &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+// U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_FUN_FLIP \
@@ -395,13 +395,13 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 
 #define MIRYOKU_ALTERNATIVES_NUM \
-&kp GRAVE,         &kp SEMI,          &kp MINUS,         &kp PLUS,          &kp PIPE           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp GRAVE,         &kp SEMI,          &kp MINUS,         &kp PLUS,          &kp PIPE,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp N6,            &kp N7,            &kp N8,            &kp N9,            &kp N0,            U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LBKT,          &kp RBKT,          &kp DOT,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-// #define MIRYOKU_ALTERNATIVES_SYM \
-// &kp LS(GRAVE),         &kp LS(SEMI),          &kp LS(MINUS),         &kp LS(PLUS),          &kp LS(PIPE)           U_NA,              U_NA,              &kp UP,            U_NA,              U_NA,            \
-// &kp LS(N1),            &kp LS(N2),            &kp LS(N3),            &kp LS(N4),            &kp LS(N5),            U_NA,              &kp LEFT,          &kp DOWN,          &kp RIGHT,         U_NA,            \
-// &kp LS(N6),            &kp LS(N7),            &kp LS(N8),            &kp LS(N9),            &kp LS(N0),            U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,        \
-// U_NP,                  U_NP,                  &kp LS(LBKT),          &kp LS(RBKT),          &kp LS(DOT),           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+#define MIRYOKU_ALTERNATIVES_SYM \
+&kp LS(GRAVE),         &kp LS(SEMI),          &kp LS(MINUS),         &kp LS(PLUS),          &kp LS(PIPE)           U_NA,              U_NA,              &kp UP,            U_NA,              U_NA,            \
+&kp LS(N1),            &kp LS(N2),            &kp LS(N3),            &kp LS(N4),            &kp LS(N5),            U_NA,              &kp LEFT,          &kp DOWN,          &kp RIGHT,         U_NA,            \
+&kp LS(N6),            &kp LS(N7),            &kp LS(N8),            &kp LS(N9),            &kp LS(N0),            U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,        \
+U_NP,                  U_NP,                  &kp LS(LBKT),          &kp LS(RBKT),          &kp LS(DOT),           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
