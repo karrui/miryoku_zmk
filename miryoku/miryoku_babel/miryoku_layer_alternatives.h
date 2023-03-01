b3411d28297ca1c716e99dec36bfa64634af6b84
Kar Rui Lau <karrui@open.gov.sg>
2023-03-01 17:37:39 +0800

    feat: ANOTHER ONE

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -401,7 +401,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 U_NP,              U_NP,              &kp LBKT,          &kp RBKT,          &kp DOT,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_SYM \
-&kp LS(GRAVE),         &kp LS(SEMI),          &kp LS(MINUS),         &kp LS(PLUS),          &kp LS(PIPE)           U_NA,              U_NA,              &kp UP,            U_NA,              U_NA,            \
+&kp LS(GRAVE),         &kp LS(SEMI),          &kp LS(MINUS),         &kp LS(PLUS),          &kp LS(PIPE),           U_NA,              U_NA,              &kp UP,            U_NA,              U_NA,            \
 &kp LS(N1),            &kp LS(N2),            &kp LS(N3),            &kp LS(N4),            &kp LS(N5),            U_NA,              &kp LEFT,          &kp DOWN,          &kp RIGHT,         U_NA,            \
 &kp LS(N6),            &kp LS(N7),            &kp LS(N8),            &kp LS(N9),            &kp LS(N0),            U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,        \
 U_NP,                  U_NP,                  &kp LS(LBKT),          &kp LS(RBKT),          &kp LS(DOT),           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
